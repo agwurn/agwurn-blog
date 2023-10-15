9f7f94e724d00430cacd4a41d1d5a2262a5da83b
lu_mbp <48710248+agwurn@users.noreply.github.com>
2023-10-15 13:46:53 +0800

    first commit

---
+++ pages/index.js
@@ -0,0 +1,59 @@
+import Head from 'next/head';
+import Link from 'next/link';
+import Layout, { siteTitle } from '../components/layout';
+import utilStyles from '../styles/utils.module.css';
+import { getSortedPostsData } from '../lib/posts';
+
+const myIntroduction = "web dev. / music prod."
+const myQuote = "practice makes perfect"
+
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData();
+  return {
+    props: {
+      allPostsData,
+    },
+  };
+}
+
+export default function Home({ allPostsData }) {
+  return (
+    <Layout home>
+      <Head>
+        <title>{siteTitle}</title>
+      </Head>
+      <section className={utilStyles.headingMd}>
+        <p>{myIntroduction}</p>
+        <p>
+          {myQuote}
+        </p>
+      </section>
+
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              <Link href={`/posts/${id}`}>{title}</Link>
+              <br />
+              <small className={utilStyles.lightText}>
+                <Date dateString={date} />
+              </small>
+            </li>
+          ))}
+        </ul>
+      </section>
+    </Layout>
+  );
+}
+
+/*
+
+getServerSideProps only if you need to pre-render a page 
+  whose data must be fetched at request time.
+
+Time to first byte (TTFB) will be slower than getStaticProps 
+  because the server must compute the result on every request,
+  and the result cannot be cached by a CDN without extra configuration.
+
+*/
