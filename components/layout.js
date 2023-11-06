bd2b97b4838b21cd03455d1f3703089fc06fe300
lu_mbp <48710248+agwurn@users.noreply.github.com>
2023-11-06 11:27:26 +0800

    feature| new favicon, thumbnail, description

--- components/layout.js
+++ components/layout.js
@@ -14,13 +14,11 @@ export default function Layout({ children }) {
         <link rel="icon" href="/favicon.ico" />
         <meta
           name="description"
-          content="Learn how to build a personal website using Next.js"
+          content="理鈞的部落格，紀錄前後端開發、音樂、羽球筆記。"
         />
         <meta
           property="og:image"
-          content={`https://og-image.vercel.app/${encodeURI(
-            siteTitle,
-          )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
+          content="/images/thumbnail.png"
         />
         <meta name="og:title" content={siteTitle} />
         <meta name="twitter:card" content="summary_large_image" />
